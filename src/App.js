8a109bf9fe3e64e9a57d61d490512039061b7a51
Omer Melih Savas <omermelihsavas@outlook.com>
2023-04-08 00:29:15 +0300

    first day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { usePosition } from 'use-position';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Weather from './components/weather-comp/Weather';
 
 function App() {
+  const { latitude, longitude } = usePosition();
+  const [weatherData, setWeatherData] = useState();
+
+  const GetWeatherData = async (lat, lon) => {
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+    try {
+      const { data } = await axios.get(`${BASE_URL}lat=${lat}&lon=${lon}&lang=tr&appid=${API_KEY}&units=metric`);
+      setWeatherData(data);
+    } catch {
+      alert("Veriler çekilemedi");
+    }
+  }
+
+  useEffect(() => {
+    latitude && longitude && GetWeatherData(latitude, longitude);
+  }, [latitude, longitude])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-side">
+        <Weather data={weatherData}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/weather-comp/Weather.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Weather.css'
+function Weather({ data }) { 
+
+  return (
+    <div className='weather'>
+      <div className="icon-description">
+      
+      </div>
+
+      <div className="location-temp">
+
+      </div>
+    </div>
+  )
+}
+
+export default Weather
